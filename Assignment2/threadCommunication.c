831954881ae2a1aa99ff8c1b6e6c51b8c3af634e
Owen <oduffy98@gmail.com>
2018-02-14 19:48:41 +0000

    Revised

--- Assignment2/threadCommunication.c
+++ Assignment2/threadCommunication.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+struct consumerInput
+{
+	int index;
+	char** readLocation;
+	char** writeLocation;
+}consumerInput;
+
+void *readMessage(void* P)
+{
+	struct consumerInput *info = P;
+}
+
+void *printMessage(void* msg)
+{
+	while(1)
+	{
+		if(msg != NULL)
+		{
+			printf("%s\n", (char*) msg);
+		}
+	}
+}
 
 int main(int argc, char** argv)
 {
-	
+	pthread_t threads[3];
+	int rc,index;
+	char mainToConsumer[16000];
+	char consumerToPrinter[16000];
+
+	for (index=0;index<3;index++) {
+		struct consumerInput parameters = {.index = index, .readLocation = &mainToConsumer,
+			.writeLocation = &consumerToPrinter};
+			rc = pthread_create(&threads[index],NULL,readMessage,(void *) &parameters);
+			if (rc) {
+				printf("ERROR return code from pthread_create(): %d\n",rc);
+				exit(-1);
+			}
+	}
+	rc = 0;
+	rc = pthread_create(&threads[3],NULL,printMessage,(void *) &consumerToPrinter);
+	if (rc) {
+		printf("ERROR return code from pthread_create(): %d\n",rc);
+		exit(-1);
+	}
+
+	// wait for threads to exit
+	for(index=0;index<4;index++) {
+		pthread_join( threads[index], NULL);
+	}
+	return 0;
+
 }
